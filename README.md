89a725615149637e3f8c762c9798cc8584ae870e
SpeedySan <akshat.sh@gmail.com>
2021-06-29 00:17:38 -0700

    Working commit.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # id3-to-directories
-A tool to organize a directory of unsorted (but ID3 tagged) music files into "Artist Name/Album Name/Song Title.ext" sub-directory structure under a location specified by an environment variable.
+A command-line tool to organize a directory of unsorted (but ID3 tagged) music files into "Artist Name/Album Name/Song Title.ext" sub-directory structure under a location specified by an environment variable.
+
+We operate with the assumption that most users are going to have a primary "music directory" on their computer. For instance on linux environments, this will often be `$HOME/Music`. We use this assumption to make the operation of this tool as simple as possible where you have to supply a single argument, the source directory which contains unsorted (but ID3 tagged) files.
+
+I wrote this package over a few minutes while playing with the excellent node-id3 package and as such this tool addresses a limited use-case. But this being a GPLV3 package, feel free to submit PRs or fork the repo to support your own use case.
+
+## Installation
+
+```
+npm i -g id3-to-directories
+```
+
+## Usage
+
+### Set an environment variable
+
+In your .bashrc (or equivalent) export the variable `MUSIC_STORAGE_DIRECTORY`. For instance,
+
+```sh
+export MUSIC_STORAGE_DIRECTORY=/home/adamsmith/Music
+```
+
+### Invoke the command
+
+```sh
+id3-to-directories /home/adamsmith/Downloads
+```
+
+This will cause all the mp3 files present in `/home/adamsmith/Downloads` (and sub-directories) to be moved into corresponding `/home/adamsmith/Music/<artist name>/<album name>/<song title>.<extension>` directories.
+
+## Credits
+
+The real magic of reading ID3 tags is performed by the [node-id3 package](https://github.com/Zazama/node-id3). I only wrote some very simple FS walking logic which uses the said package.
---
+++ index.js
@@ -0,0 +1,134 @@
+#! /usr/bin/env node
+'use strict';
+const { promises: fs } = require('fs');
+const path = require('path');
+const { Promise: NodeID3 } = require('node-id3');
+
+const srcDir = process.argv[2];
+const targetDirectory = process.env.MUSIC_STORAGE_DIRECTORY;
+
+const unsafe = ['$RECYCLE.BIN'];
+const isSafe = (root, entry) => unsafe.indexOf(entry.name) === -1;
+
+/*
+ * Get a flat-list of fs.Dirent objects. Each such object is augmented with the full path to simplify things later.
+ */
+const getFiles = async (root) => {
+  const entries = await fs.readdir(root, { withFileTypes: true });
+  const files = [];
+  for (let entry of entries) {
+    if (isSafe(root, entry)) {
+      if (entry.isFile()) {
+        entry.filePath = path.join(root, entry.name);
+        files.push(entry);
+      } else if (entry.isDirectory()) {
+        files.push(...await getFiles(path.join(root, entry.name)));
+      }
+    }
+  }
+
+  return files;
+};
+
+const printUsage = () => {
+  const usage = [
+    'id3-to-directories',
+    'A tool to organize a directory of unsorted (but ID3 tagged) music files into "$MUSIC_STORAGE_DIRECTORY/Artist Name/Album Name/Song Title.ext" directory structure.',
+    '  where MUSIC_STORAGE_DIRECTORY is an environment variable.',
+    '\n',
+    'Usage:',
+    '  id3-to-directories SrcDirectoryPath',
+    'e.g.',
+    '  # The following case where all mp3 files in ~/Downloads will be moved to the right sub-directories under /home/adamsmith/Music',
+    '  in your .bashrc (or .zshrc)',
+    '  export MUSIC_STORAGE_DIRECTORY=/home/adamsmith/Music',
+    '  id3-to-directories ~/Downloads',
+    new Array(20).fill('-').join(''),
+  ].join('\n');
+  console.log(usage);
+}
+
+const main = async () => {
+  try {
+    // Check pre-reqs
+    if (!srcDir) {
+      throw new Error('Missing srcDir.');
+    };
+
+    if (!targetDirectory) {
+      throw new Error('Missing MUSIC_STORAGE_DIRECTORY environment variable.');
+    }
+
+    const srcDirEntry = await fs.stat(srcDir);
+    if (!srcDirEntry.isDirectory()) {
+      throw new Error(`${srcdir} is not a directory.`);
+    }
+
+    const targetDirEntry = await fs.stat(targetDirectory);
+    if (!targetDirEntry.isDirectory()) {
+      throw new Error(`${targetDirEntry} is not a directory.`);
+    }
+  } catch (error) {
+    printUsage();
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Get list of files.
+  console.log(`Resursively scanning ${srcDir} to get list of files...`);
+  const fileEntries = await getFiles(srcDir);
+  console.log(`${fileEntries.length} files found.`);
+  if (fileEntries.length) {
+    console.log('Reading ID3 tags...');
+    // Organize as Artist { Album { Title }}
+    const byArtist = {};
+    for (let fileEntry of fileEntries) {
+      const { filePath } = fileEntry;
+      const tags = await NodeID3.read(filePath);
+      const {
+        artist = "Unknown Artist",
+        album = "Unknown Album",
+        title = path.basename(filePath),
+      } = tags;
+
+      let byAlbum = byArtist[artist];
+      if (!byAlbum) {
+        byAlbum = byArtist[artist] = {};
+      }
+
+      let albumSongs = byAlbum[album];
+      if (!albumSongs) {
+        albumSongs = byAlbum[album] = {};
+      }
+
+      albumSongs[title] = fileEntry;
+    }
+
+    console.log('Done reading ID3. Moving files...');
+    // Move files to the right directory
+    for (let artist in byArtist) {
+      // console.log(artist);
+      for (let album in byArtist[artist]) {
+        // console.log('\t', album);
+        for (let title in byArtist[artist][album]) {
+          // console.log('\t\t', title);
+          const { filePath } = byArtist[artist][album][title];
+          const finalTargetDirectory = path.join(targetDirectory, artist, album);
+          const targetFilePath = `${path.join(finalTargetDirectory, title)}${path.extname(filePath)}`;
+          if (targetFilePath !== filePath) {
+            // console.log(filePath, '--->---', targetFilePath);
+            console.log(`mkdir ${finalTargetDirectory}`);
+            await fs.mkdir(finalTargetDirectory, { recursive: true });
+            console.log(`mv ${filePath} ${targetFilePath}`);
+            await fs.rename(filePath, targetFilePath);
+          }
+        }
+      }
+    }
+    console.log('All done.');
+  } else {
+    console.log('Nothing to do.');
+  }
+};
+
+main();
